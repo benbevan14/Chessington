a89999722cc7966933fa26d22df77016ce1995e5
Benjamin Bevan <bbevan196@gmail.com>
2021-09-22 16:12:52 +0100

    pawns can now move

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -1,16 +1,27 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Chessington.GameEngine.Pieces
 {
     public class Pawn : Piece
     {
-        public Pawn(Player player) 
+        public Pawn(Player player)
             : base(player) { }
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            Square currentSquare = board.FindPiece(this);
+
+            switch (this.Player)
+            {
+                case Player.White:
+                    return new List<Square> { Square.At(currentSquare.Row - 1, currentSquare.Col) };
+                case Player.Black:
+                    return new List<Square> { Square.At(currentSquare.Row + 1, currentSquare.Col) };
+                default:
+                    return new List<Square>();
+            }
         }
     }
 }
