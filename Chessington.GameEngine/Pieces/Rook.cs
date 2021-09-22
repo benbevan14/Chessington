ffe9475c98de6f4d32efac6c96b504bb71042ae0
Benjamin Bevan <bbevan196@gmail.com>
2021-09-22 17:56:19 +0100

    rook movement

--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -10,7 +10,19 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            Square currentSquare = board.FindPiece(this);
+
+            List<Square> newMoves = new List<Square>();
+
+            for (int i = 0; i < 8; i++)
+            {
+                newMoves.Add(Square.At(currentSquare.Row, i));
+                newMoves.Add(Square.At(i, currentSquare.Col));
+            }
+
+            newMoves.RemoveAll(s => s == Square.At(currentSquare.Row, currentSquare.Col));
+
+            return newMoves;
         }
     }
 }
