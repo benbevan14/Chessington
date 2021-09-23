48dbf3175bfe77ac692855a1f65f11ab94c9888b
Benjamin Bevan <bbevan196@gmail.com>
2021-09-23 10:19:39 +0100

    king movement

--- Chessington.GameEngine/Pieces/King.cs
+++ Chessington.GameEngine/Pieces/King.cs
@@ -10,7 +10,21 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            Square currentSquare = board.FindPiece(this);
+
+            List<Square> newMoves = new List<Square>();
+
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    newMoves.Add(Square.At(currentSquare.Row + i, currentSquare.Col + j));
+                }
+            }
+
+            newMoves.RemoveAll(s => s == Square.At(currentSquare.Row, currentSquare.Col));
+
+            return newMoves;
         }
     }
 }
