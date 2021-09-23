f1685547805d5d3ba588393120737a59cb34bc7b
Benjamin Bevan <bbevan196@gmail.com>
2021-09-23 10:05:02 +0100

    knight moves working

--- Chessington.GameEngine/Pieces/Knight.cs
+++ Chessington.GameEngine/Pieces/Knight.cs
@@ -10,7 +10,21 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            Square currentSquare = board.FindPiece(this);
+
+            List<Square> newMoves = new List<Square>();
+
+            int[] xMoves = new[] {-2, 2, 2, -2, -1, 1, 1, -1};
+            int[] yMoves = new[] {-1, 1, -1, 1, 2, 2, -2, -2};
+
+            for (int i = 0; i < xMoves.Length; i++)
+            {
+                newMoves.Add(Square.At(currentSquare.Row + yMoves[i], currentSquare.Col + xMoves[i]));
+            }
+
+            newMoves.RemoveAll(s => (s.Row < 0 && s.Row > 7) || (s.Col < 0 && s.Col > 7));
+
+            return newMoves;
         }
     }
 }
