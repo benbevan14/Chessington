93206872040395f54e6d92f91a74eaeb4c1188a0
Benjamin Bevan <bbevan196@gmail.com>
2021-09-23 10:55:15 +0100

    pawns can be blocked

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -15,22 +15,21 @@ namespace Chessington.GameEngine.Pieces
 
             List<Square> newMoves = new List<Square>();
 
-            if (Player == Player.White)
-            {
-                newMoves.Add(Square.At(currentSquare.Row - 1, currentSquare.Col));
-                if (currentSquare.Row == 6)
-                {
-                    newMoves.Add(Square.At(currentSquare.Row - 2, currentSquare.Col));
-                }
-            }
-            else if (Player == Player.Black)
-            {
-                newMoves.Add(Square.At(currentSquare.Row + 1, currentSquare.Col));
-                if (currentSquare.Row == 1)
-                {
-                    newMoves.Add(Square.At(currentSquare.Row + 2, currentSquare.Col));
-                }
-            }
+            int startRow = this.Player == Player.White ? 6 : 1;
+            int dir = this.Player == Player.White ? -1 : 1;
+
+            // If there's a piece in front of the pawn it can't move, so return empty list
+            if (board.GetPiece(Square.At(currentSquare.Row + dir, currentSquare.Col)) != null) return newMoves;
+
+            // Add the square in front of the pawn as a possible move
+            newMoves.Add(Square.At(currentSquare.Row + dir, currentSquare.Col));
+
+            // If it's not the first move return the list of just one move
+            if (currentSquare.Row != startRow) return newMoves;
+
+            // If it's the first move and the square two in front is empty, add that move
+            if (board.GetPiece(Square.At(currentSquare.Row + (dir * 2), currentSquare.Col)) == null)
+                newMoves.Add(Square.At(currentSquare.Row + (dir * 2), currentSquare.Col));
 
             return newMoves;
         }
