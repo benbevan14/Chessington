0f5e3780a88a0193883e27dc3e083e230ceea2cf
Benjamin Bevan <bbevan196@gmail.com>
2021-09-23 09:38:48 +0100

    queen movement working

--- Chessington.GameEngine/Pieces/Queen.cs
+++ Chessington.GameEngine/Pieces/Queen.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Chessington.GameEngine.Pieces
@@ -10,7 +11,29 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            Square currentSquare = board.FindPiece(this);
+
+            List<Square> newMoves = new List<Square>();
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (Math.Abs(i - currentSquare.Row) == Math.Abs(j - currentSquare.Col))
+                    {
+                        newMoves.Add(Square.At(i, j));
+                    }
+
+                    if (i == currentSquare.Row || j == currentSquare.Col)
+                    {
+                        newMoves.Add(Square.At(i, j));
+                    }
+                }
+            }
+
+            newMoves.RemoveAll(s => s == Square.At(currentSquare.Row, currentSquare.Col));
+
+            return newMoves;
         }
     }
 }
